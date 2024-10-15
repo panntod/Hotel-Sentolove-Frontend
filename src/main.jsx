349bf79f5109e8fe2299ff2805bb4c1abc39ebca
panntod <munjalindra.pandhu@gmail.com>
2024-10-15 08:15:02 +0700

    Refactor main.jsx to use StrictMode and remove unused imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,18 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+import { StrictMode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "./utils/store/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <StrictMode>
     <Provider store={store}>
       <ChakraProvider>
         <App />
       </ChakraProvider>
     </Provider>
-  </React.StrictMode>
+  </StrictMode>,
 );
