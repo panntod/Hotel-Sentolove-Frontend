d2f6b8ef42c3f4694f9406c6fb0a0baa1f49c9e4
panntod <munjalindra.pandhu@gmail.com>
2024-10-26 05:25:09 +0700

    refactor: Update start script in package.json and remove unused code in constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,21 +1,7 @@
-import { getLocalStorage } from "./helper/localStorage";
-
 const BASE_API = "http://localhost:8000";
 const BASE_API_IMAGE = "http://localhost:8000/public";
 
-const LOCAL_STORAGE_TOKEN = "sentolove/token";
 const LOCAL_STORAGE_USER = "sentolove/user";
+const LOCAL_STORAGE_TOKEN = "sentolove/token";
 
-const TOKEN = {
-  headers: {
-    Authorization: `Bearer ${getLocalStorage(LOCAL_STORAGE_TOKEN)}`,
-  },
-};
-
-export {
-  BASE_API,
-  BASE_API_IMAGE,
-  LOCAL_STORAGE_TOKEN,
-  LOCAL_STORAGE_USER,
-  TOKEN,
-};
+export { BASE_API, BASE_API_IMAGE, LOCAL_STORAGE_USER, LOCAL_STORAGE_TOKEN };
