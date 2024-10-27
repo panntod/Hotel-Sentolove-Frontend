5304df85383a9950ed68cf61414fbfe538325331
panntod <munjalindra.pandhu@gmail.com>
2024-10-27 19:30:56 +0700

    refactor: Remove StrictMode from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,15 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-import { StrictMode } from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { Provider } from "react-redux";
 import { store } from "./utils/store/store";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <ChakraProvider>
-        <App />
-      </ChakraProvider>
-    </Provider>
-  </StrictMode>,
+  <Provider store={store}>
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </Provider>,
 );
